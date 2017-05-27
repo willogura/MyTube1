b9b607b9ed8cb22c2352378609104af08ca60d30
William <wogura@me.com>
2017-05-27 11:28:06 -0500

    Initial Commit

---
+++ MyTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTube
+//
+//  Created by William Ogura on 5/27/17.
+//  Copyright © 2017 William Ogura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
